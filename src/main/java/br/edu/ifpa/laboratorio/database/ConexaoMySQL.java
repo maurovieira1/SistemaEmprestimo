354ff98833feed9d318a648d27d7976f2ac775a0
maurovieira1 <mauro.tuc9@gmail.com>
2026-05-12 19:57:29 -0300

    Segurança: Atualizando conexão e removendo senhas

--- src/main/java/br/edu/ifpa/laboratorio/database/ConexaoMySQL.java
+++ src/main/java/br/edu/ifpa/laboratorio/database/ConexaoMySQL.java
@@ -2,14 +2,30 @@ package br.edu.ifpa.laboratorio.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.Properties;
+import java.io.FileInputStream;
 
 public class ConexaoMySQL {
-    private static final String URL = "jdbc:mysql://localhost:3306/controle_laboratorio";
-    private static final String USER = "root";
-    private static final String PASS = "1212";
 
-    public static Connection getConexao() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+    public static Connection getConexao() {
+        Properties props = new Properties();
+        try {
+            // Busca o arquivo config.properties que está na raiz do seu projeto
+            FileInputStream fis = new FileInputStream("config.properties");
+            props.load(fis);
+            fis.close();
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String pass = props.getProperty("db.password");
+
+            // Carrega o driver do MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar conexão: " + e.getMessage());
+            return null;
+        }
     }
 }
